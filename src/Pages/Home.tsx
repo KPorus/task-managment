ab36d8c5f6eb5ece8b77f497146d1d5fb17972c2
KPorus <designerfardin2021@gmail.com>
2022-12-27 12:36:13 +0600

    home page update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Home = () => {
     return (
-        <div className="relative bg-deep-purple-accent-400">
+        <div className="relative bg-deep-purple-accent-400 flex">
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
                     <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                        The quick, brown fox
+                        The quick, fast 
                         <br className="hidden md:block" />
-                        jumps over a{' '}
+                        & easy to use{' '}
                         <span className="relative inline-block px-2">
                             <div className="absolute inset-0 transform -skew-x-12 bg-teal-accent-400" />
-                            <span className="relative text-teal-900">lazy dog</span>
+                            <span style={{ color: "#4285f4", fontWeight: "bold" }}>
+                                <Typewriter
+                                    words={["to track your task"]}
+                                    loop={1000}
+                                    cursor
+                                    cursorStyle='_'
+                                    typeSpeed={100}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                />
+                            </span>
                         </span>
                     </h2>
                     <p className="mb-6 text-base text-indigo-100 md:text-lg">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae. explicabo. Sed ut perspiciatis unde omnis.
-                    </p>
-                    <p className="max-w-md mb-10 text-xs tracking-wide text-indigo-100 sm:text-sm sm:mx-auto md:mb-16">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                        accusantium doloremque.
+                        Plan, organize, and collaborate on any project with powerful task management that can be customized for every need.
                     </p>
                     <Link
                         to="/signUp"
