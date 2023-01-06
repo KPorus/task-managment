14397d877aec361086da79f451a06de3efad724a
KPorus <designerfardin2021@gmail.com>
2023-01-06 17:47:13 +0600

    login to home

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { AuthContext } from '../component/context/AuthProvider/AuthProvider';
 import Nav from '../component/Nav';
+import Login from '../Pages/Login';
 
 const Main = () => {
+    const {user} = useContext(AuthContext)
     return (
         <div className='bg-gray-900 min-h-screen p-4'>
             <Nav></Nav>
-            <div className='min-h-screen'>
-            <Outlet></Outlet>
-            </div>
+            {
+                user ? <div className='min-h-screen'>
+                    <Outlet></Outlet>
+                </div> : <Login></Login>
+            }
         </div>
     );
 };
